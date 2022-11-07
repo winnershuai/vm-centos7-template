cc5429fb783648d8b01076601644adf5b5af2cf6
winnershuai <51901340+winnershuai@users.noreply.github.com>
2022-11-07 21:43:06 +0800

    Update template.sh

--- template.sh
+++ template.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -e
+#设置主机名和host
 hostnamectl set-hostname template
 echo "127.0.0.1 $(hostname)" >> /etc/hosts
+#关闭防火墙
 sed -i "s/^SELINUX=.*/SELINUX=disabled/" /etc/selinux/config 
 setenforce 0
 systemctl stop firewalld
 systemctl disable firewalld
+#设置网卡配置
 cat > /etc/sysconfig/network-scripts/ifcfg-ens192 << EOF 
 TYPE=Ethernet
 BOOTPROTO=static
@@ -16,17 +19,20 @@ GATEWAY=192.168.1.1
 DNS1=114.114.114.114
 DNS2=8.8.8.8
 EOF
+#设置ssh配置
 echo 'UseDNS no'>>/etc/ssh/sshd_config
 echo 'PermitRootLogin yes'>>/etc/ssh/sshd_config
 systemctl status sshd
 /etc/init.d/network restart
 #设置阿里源
-curl -o /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-7.repo 
+curl -o /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-7.repo
 yum clean all
 yum makecache 
+#下载ntp设置对时
 yum install ntp -y
 ntpdate ntp.aliyun.com
 hwclock -w
+yum install epel-release
 yum install -y bash-completion net-tools
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
